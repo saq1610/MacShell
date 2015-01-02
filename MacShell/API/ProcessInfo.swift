3b618c7b17ce8608c2d873bdfc154c5b1b2b881f
Florian <haemmerle.florian@outlook.com>
2015-01-02 12:22:55 +0100

    Restructured ProcessInfo API

--- MacShell/API/ProcessInfo.swift
+++ MacShell/API/ProcessInfo.swift
@@ -25,19 +25,19 @@ class ProcessInfo: NSObject, APIPackage {
     func processMessage(message: WKScriptMessage) {
         switch (message.name) {
         case "getProcessName":
-            message.webView?.evaluateJavaScript("console.log('\(NSProcessInfo.processInfo().processName)')", completionHandler: nil)
+            message.webView?.evaluateJavaScript("console.log('\(processName)')", completionHandler: nil)
             break
         case "getProcessIdentifier":
-            message.webView?.evaluateJavaScript("console.log(\(NSProcessInfo.processInfo().processIdentifier))", completionHandler: nil)
+            message.webView?.evaluateJavaScript("console.log(\(processIdentifier))", completionHandler: nil)
             break
         case "getProcessorCount":
-            message.webView?.evaluateJavaScript("console.log(\(NSProcessInfo.processInfo().processorCount))", completionHandler: nil)
+            message.webView?.evaluateJavaScript("console.log(\(processorCount))", completionHandler: nil)
             break
         case "getActiveProcessorCount":
-            message.webView?.evaluateJavaScript("console.log(\(NSProcessInfo.processInfo().activeProcessorCount))", completionHandler: nil)
+            message.webView?.evaluateJavaScript("console.log(\(activeProcessorCount))", completionHandler: nil)
             break
         case "getHostName":
-            message.webView?.evaluateJavaScript("console.log('\(NSProcessInfo.processInfo().hostName)')", completionHandler: nil)
+            message.webView?.evaluateJavaScript("console.log('\(hostName)')", completionHandler: nil)
             break
         case "getOperatingSystemVersion":
             var osVersion = NSProcessInfo.processInfo().operatingSystemVersion
@@ -45,16 +45,64 @@ class ProcessInfo: NSObject, APIPackage {
             message.webView?.evaluateJavaScript("console.log(JSON.parse('\(osVersionString)'))", completionHandler: nil)
             break;
         case "getPhysicalMemory":
-            message.webView?.evaluateJavaScript("console.log(\(NSProcessInfo.processInfo().physicalMemory))", completionHandler: nil)
+            message.webView?.evaluateJavaScript("console.log(\(physicalMemory))", completionHandler: nil)
             break
         case "getSystemUptime":
-            message.webView?.evaluateJavaScript("console.log(\(NSProcessInfo.processInfo().systemUptime))", completionHandler: nil)
+            message.webView?.evaluateJavaScript("console.log(\(systemUptime))", completionHandler: nil)
             break
         case "getGloballyUniqueString":
-            message.webView?.evaluateJavaScript("console.log('\(NSProcessInfo.processInfo().globallyUniqueString)')", completionHandler: nil)
+            message.webView?.evaluateJavaScript("console.log('\(globallyUniqueString)')", completionHandler: nil)
             break
         default:
             break
+        }
+    }
+    
+    var processName: String {
+        get {
+            return NSProcessInfo.processInfo().processName
+        }
+    }
+    
+    var processIdentifier: Int32 {
+        get {
+            return NSProcessInfo.processInfo().processIdentifier
+        }
+    }
+    
+    var processorCount: Int {
+        get {
+            return NSProcessInfo.processInfo().processorCount
+        }
+    }
+    
+    var activeProcessorCount: Int {
+        get {
+            return NSProcessInfo.processInfo().activeProcessorCount
+        }
+    }
+    
+    var hostName: String {
+        get {
+            return NSProcessInfo.processInfo().hostName
+        }
+    }
+    
+    var physicalMemory: UInt64 {
+        get {
+            return NSProcessInfo.processInfo().physicalMemory
+        }
+    }
+    
+    var systemUptime: Double {
+        get {
+            return NSProcessInfo.processInfo().systemUptime
+        }
+    }
+    
+    var globallyUniqueString: String {
+        get {
+            return NSProcessInfo.processInfo().globallyUniqueString
         }
     }
 }
