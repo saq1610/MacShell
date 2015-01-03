09c0f327fec45b72dfc9551b5d733c6eabc7a6ab
Florian <haemmerle.florian@outlook.com>
2015-01-03 09:07:33 +0100

    FileSystem API

--- MacShell/API/FileSystem.swift
+++ MacShell/API/FileSystem.swift
@@ -11,10 +11,24 @@ import WebKit
 
 class FileSystem: NSObject, APIPackage {
     func registerMethods(handler: WKScriptMessageHandler, webView: WKWebView) {
-        
+        webView.configuration.userContentController.addScriptMessageHandler(handler, name: "doesFileExist")
     }
     
     func processMessage(message: WKScriptMessage) {
-        
+        switch (message.name) {
+            case "doesFileExist":
+                var path: String? = message.body as? String
+                if (path != nil) {
+                    message.webView?.evaluateJavaScript("console.log('\(doesFileExist(path!))')", completionHandler: nil)
+                }
+                break
+            
+        default:
+            break
+        }
+    }
+    
+    func doesFileExist(path: String) -> Bool {
+        return NSFileManager.defaultManager().fileExistsAtPath(path)
     }
 }
