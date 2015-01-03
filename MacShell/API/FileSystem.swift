68da7097971a0edbc000d88980048268c178fc57
Florian <haemmerle.florian@outlook.com>
2015-01-03 09:12:22 +0100

    Added FileSystem.isDirectory

--- MacShell/API/FileSystem.swift
+++ MacShell/API/FileSystem.swift
@@ -12,16 +12,23 @@ import WebKit
 class FileSystem: NSObject, APIPackage {
     func registerMethods(handler: WKScriptMessageHandler, webView: WKWebView) {
         webView.configuration.userContentController.addScriptMessageHandler(handler, name: "doesFileExist")
+        webView.configuration.userContentController.addScriptMessageHandler(handler, name: "isDirectory")
     }
     
     func processMessage(message: WKScriptMessage) {
         switch (message.name) {
-            case "doesFileExist":
-                var path: String? = message.body as? String
-                if (path != nil) {
-                    message.webView?.evaluateJavaScript("console.log('\(doesFileExist(path!))')", completionHandler: nil)
-                }
-                break
+        case "doesFileExist":
+            var path: String? = message.body as? String
+            if (path != nil) {
+                message.webView?.evaluateJavaScript("console.log('\(doesFileExist(path!))')", completionHandler: nil)
+            }
+            break
+        case "isDirectory":
+            var path: String? = message.body as? String
+            if (path != nil) {
+                message.webView?.evaluateJavaScript("console.log('\(isDirectory(path!))')", completionHandler: nil)
+            }
+            break
             
         default:
             break
@@ -30,5 +37,11 @@ class FileSystem: NSObject, APIPackage {
     
     func doesFileExist(path: String) -> Bool {
         return NSFileManager.defaultManager().fileExistsAtPath(path)
+    }
+    
+    func isDirectory(path: String) -> Bool {
+        var isDir = ObjCBool(false)
+        NSFileManager.defaultManager().fileExistsAtPath(path, isDirectory: &isDir)
+        return isDir.boolValue
     }
 }
